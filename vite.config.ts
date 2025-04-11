6692778cb10021bf87f24b34f3f1eed673553230
Lorenzo Campioni <lollopod@users.noreply.github.com>
2025-04-11 11:32:06 +0000

    Including all the files

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: baby-announcement
+  base: '/baby-announcement/',
   plugins: [react()],
 });
